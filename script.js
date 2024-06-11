0fd816882d8c1fdacccdbaebda3dbab48d776d76
Mathacevedo <130381642+Mathacevedo@users.noreply.github.com>
2024-06-11 08:38:07 -0300

    e

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+            alternativas: [
+            {
+            texto:"Alternativa 1",
+            afirmação:"afirmação",
+            },
+            {
+            texto:"Alternativa 2",
+            afirmação:"afirmção",
+            },
+
+
+
+        ]
+    },]
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
